11257b2cb3a7b372f403ae564c99899dee38ac20
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-08-19 15:32:42 -0300

    removendo log

--- webhook-spring-boot-prod/src/main/java/com/elbertribeiro/alerta/AlertaService.java
+++ webhook-spring-boot-prod/src/main/java/com/elbertribeiro/alerta/AlertaService.java
@@ -2,7 +2,6 @@ package com.elbertribeiro.alerta;
 
 import com.elbertribeiro.comum.WebHookHttpClient;
 import com.elbertribeiro.webhook.WebhookService;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
