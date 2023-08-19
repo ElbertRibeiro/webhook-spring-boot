30d66c410ba4d5837b50017a1c23ebd6accc8f97
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-08-19 16:45:37 -0300

    recebendo um dto para enviar em um evento

--- webhook-spring-boot-prod/src/main/java/com/elbertribeiro/alerta/AlertaController.java
+++ webhook-spring-boot-prod/src/main/java/com/elbertribeiro/alerta/AlertaController.java
@@ -1,21 +1,25 @@
 package com.elbertribeiro.alerta;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.elbertribeiro.dto.ProtocoloDto;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping("alerta")
+@Log4j2
 public class AlertaController {
-    @Autowired
-    AlertaService alertaService;
+    private final AlertaService alertaService;
 
-    @GetMapping
-    public ResponseEntity<AlertaDto> getAlerta() {
+    public AlertaController(AlertaService alertaService) {
+        this.alertaService = alertaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AlertaDto> getAlerta(@RequestBody ProtocoloDto protocoloDto) {
+        log.info(protocoloDto);
         return ok(alertaService.enviaAlerta());
     }
 }
