a52746efac48c7723ed1f5f60a27704199d5a6de
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-08-19 11:28:24 -0300

    tratando requsição no momento da falha

--- webhook-spring-boot-prod/src/main/java/com/elbertribeiro/alerta/AlertaService.java
+++ webhook-spring-boot-prod/src/main/java/com/elbertribeiro/alerta/AlertaService.java
@@ -2,19 +2,26 @@ package com.elbertribeiro.alerta;
 
 import com.elbertribeiro.comum.WebHookHttpClient;
 import com.elbertribeiro.webhook.WebhookService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Log4j2
 public class AlertaService {
     @Autowired
     WebhookService webhookService;
 
     @Autowired
     WebHookHttpClient webHookHttpClient;
-    public void enviaAlerta(){
+
+    public void enviaAlerta() {
         webhookService.findAll().forEach(webhook -> {
-            webHookHttpClient.get(webhook.getUrl(), String.class);
+            try {
+                webHookHttpClient.get(webhook.getUrl(), String.class);
+            } catch (Exception e) {
+                log.error("Erro ao fazer a requisição para: " + webhook.getUrl(), e);
+            }
         });
     }
 }
