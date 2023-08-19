39a13081b43342a7348f89db6925e0e1ebe3805a
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-08-19 09:21:21 -0300

    refatorando

--- webhook-spring-boot-prod/src/main/java/com/example/webhook/WebhookController.java
+++ webhook-spring-boot-prod/src/main/java/com/example/webhook/WebhookController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@Controller(/*"/webhooks"*/)
+@Controller
 public class WebhookController {
 
     @Autowired
